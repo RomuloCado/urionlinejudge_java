e98b5ef5a9eba3f61bd9eeaa931eb893148a7109
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 00:51:46 -0300

    Create 1005.java

---
+++ 1005.java
@@ -0,0 +1,20 @@
+//Exercicio 1005
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        
+        Scanner leitura = new Scanner(System.in);
+        double a = leitura.nextDouble();
+        double b = leitura.nextDouble();
+        double media = (a * 3.5 + b * 7.5)/11;
+        System.out.printf("MEDIA = %.5f\n", media);
+                
+        
+    }
+}
